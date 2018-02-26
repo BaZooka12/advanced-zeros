e6777b8a737b4f0e1472e78cb6389f5d7be8613d
Vladislav Ilkovec <ilkovec.vi@gmail.com>
2018-02-26 21:03:24 +0300

    Advanced-zeros: solution v 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let newNumber = [];
+  let x = number;
+
+  while (true) {
+    if (x % base < 1) {
+      break;
+    }
+    newNumber.push(x % base);
+    x = Math.floor(x / base);
+  }
+  let zeros = +newNumber.reverse().join('');
+
+  function calculateTailZeros(number) {
+    let countZeroes = 0;
+    let counter = 1;
+    while (true) {
+      let zeroes = Math.floor(number / Math.pow(5, counter));
+      if (zeroes < 1) {
+        break;
+      }
+      countZeroes += zeroes;
+      counter++;
+    }
+    return countZeroes;
+  }
+  return calculateTailZeros(zeros);
 }
